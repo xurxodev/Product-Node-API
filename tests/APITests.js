de9d7e9a2d9892b0ad85f65bf92a760da40cd69e
xurxodev <xurxosf@gmail.com>
2015-12-23 23:02:04 +0100

    add end to end test for API with mocha and supertest

---
+++ tests/APITests.js
@@ -0,0 +1,56 @@
+"use strict"
+
+ var assert = require('assert');
+ var request = require('supertest')
+ var app = require('../app.js')
+
+ var request = request("http://localhost:8080")
+
+ describe('products', function() {
+     describe('GET', function(){
+         it('Should return json as default data format', function(done){
+             request.get('/api/products')
+                 .expect('Content-Type', /json/)
+                 .expect(200, done);
+         });
+         it('Should return json as data format when set accept header to json', function(done){
+             request.get('/api/products')
+                 .set('Accept', 'application/json')
+                 .expect('Content-Type', /json/)
+                 .expect(200, done);
+         });
+     });
+     describe('POST', function(){
+         it('Should return 201 status code and location header', function(done){
+
+             let product = {sku: "ab48cicj36734",
+                            asin: "B015E8UTIU",
+                            upc: "888462500449",
+                            title: "Apple iPhone 6s 64 GB US Warranty Unlocked Cellphone - Retail Packaging (Rose Gold)",
+                            image: "http://ecx.images-amazon.com/images/I/91DpCeCgSBL._SL1500_.jpg"}
+
+             request.post('/api/products')
+                 .send(product)
+                 .expect(201)
+                 .expect('Location', '/api/products/ab48cicj36734',done);
+         });
+     });
+
+     /*        it('', function(done){
+
+      let model = new knowron ('Caffeine', 'https://en.wikipedia.org/wiki/Caffeine', 'source', 'cluster', 'description', 'image', 'target');
+
+      request("http://localhost:8080/api")
+      .post('/knowrons')
+      .send(model)
+      .expect(200, done);
+
+
+      request("http://localhost:8080/api")
+      .get('/knowrons')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+
+      });*/
+ });
